334d0b09584834d24f73c169c3573f5e261680f4
nense <nense@DESKTOP-13CC5JL>
2018-01-08 00:05:51 +0100

    Starting Dashboard

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
@@ -15,5 +17,4 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-
 }
